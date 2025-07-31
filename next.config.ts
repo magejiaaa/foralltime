a5a2cbed25d45552888b8ec090c9149107ca0030
jenny <jenny@ewebs.com.tw>
2025-07-31 18:01:02 +0800

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'www.hlr1023.huijiwiki.com',
+        hostname: 'huiji-thumb.huijistatic.com',
       }
     ]
   }
